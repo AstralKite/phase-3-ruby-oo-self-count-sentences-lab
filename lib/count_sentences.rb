725aae63d9d53924cfff6ade0ffc38fdd4affaea
Fausto Ramirez <ramirezmedia@gmail.com>
2023-04-24 18:41:14 -0400

    commit tree aed6a9b56c7d7ae87295bac80779d5e10ffc0647

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+  #  count = 0;
+  #  self.chars.each.with_index  { |char|
+  #    if (char =~ /[.?!]/)
+  #      count = count + 1
+  #    end
+  #  }
+  #  count
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    end
 end
